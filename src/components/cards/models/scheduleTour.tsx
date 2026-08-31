28834cc1c481901d3c148def8c288fd4cd3c0c8a
Soham Pirag <sohampirag@gmail.com>
2026-08-31 12:52:37 +0530

    Fix missing closing fragment tag in scheduleTour.tsx

--- src/components/cards/models/scheduleTour.tsx
+++ src/components/cards/models/scheduleTour.tsx
@@ -212,6 +212,7 @@ const ScheduleTourForm = ({
 							className="w-full px-3 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-800"
 							rows={4}></textarea>
 					</div>
+						</>
 					)}
 
 					{step === 2 && (
