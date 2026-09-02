a2a9188ccc74981c5368b7516f4d18abc2e6cf94
Sonal Pirag <sonalpirag8@gmail.com>
2026-09-02 18:25:44 +0530

    filter updated  withdrawn canceled expired

--- src/app/(public)/Florida-Real-Estate-Search/[[...slug]]/mapComponent.tsx
+++ src/app/(public)/Florida-Real-Estate-Search/[[...slug]]/mapComponent.tsx
@@ -257,7 +257,7 @@ export default function MapComponent({
 					const minY = originShift - (coord.y + 1) * tileWidth;
 					const maxY = originShift - coord.y * tileWidth;
 					const bbox = `${minX},${minY},${maxX},${maxY}`;
-					return `https://hazards.fema.gov/gis/nfhl/rest/services/public/NFHL/MapServer/export?bbox=${bbox}&bboxSR=3857&layers=show:28&size=256,256&imageSR=3857&format=png32&transparent=true&f=image`;
+					return `/api/fema?bbox=${bbox}`;
 				},
 				tileSize: new google.maps.Size(256, 256),
 				opacity: 0.5,
